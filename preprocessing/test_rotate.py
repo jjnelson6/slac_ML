8f0cbebdc0f79cb6e652bf6a673f02b049b61c25
Jovan Nelson <nelson@brux6.hep.brown.edu>
2017-04-14 23:02:32 -0400

    add

--- preprocessing/test_rotate.py
+++ preprocessing/test_rotate.py
@@ -3,7 +3,7 @@ import numpy as np
 #from matplotlib.colors import LogNorm
 import math as m
 from jettools import plot_mean_jet,flip_jet,rotate_jet
-from processing import buffer_to_jet
+#from processing import buffer_to_jet
 
 
 
@@ -15,45 +15,35 @@ def plot_mean_jet(rec, field = 'images', title = 'Average Jet Image'):
     return fig
 
 
-data=np.load("../../CMSSW_8_0_4/src/Delphes/Wprime_all.npz")
-
-temp= {}
-temp['images'] = data['image']
-temp['jet_eta']=data['jet_eta']
-temp['jet_phi']= data['jet_phi']
-
+data=np.load("../../CMSSW_8_0_4/src/Delphes/QCD_all.npz")
+print data['image'].shape
 
 rot=[]
-for k in range(len(temp['images'])):
-    e, p = (temp['jet_eta'][k], temp['jet_phi'][k])
+for k in range(len(data['image'])):
+    
+    """
+    Preforms rotation of jet using a cubic interpolation. Calculating the angle of rotation using Subleading Jets. 
+    """
+
+    if (data['SubLeadingEta'][k] < -10) | (data['SubLeadingPhi'][k] < -10):
+        e, p = (data['PCEta'][k], data['PCPhi'][k])
+    else:
+        e, p = (data['SubLeadingEta'][k], data['SubLeadingPhi'][k])
     
     angle = np.arctan(p / e) + 2.0 * np.arctan(1.0)
 
     if (-np.sin(angle) * e + np.cos(angle) * p) > 0:
         angle += -4.0 * np.arctan(1.0)
 
-    image = flip_jet(rotate_jet(np.array(temp['images'][k]), -angle, normalizer=4000.0, dim=25),'r')
-    e_norm = np.linalg.norm(image)
-   
+    image = flip_jet(rotate_jet(np.array(data['image'][k]), -angle, normalizer=4000.0, dim=25),'r')
+    e_norm = np.linalg.norm(image)   
+    
     if e_norm>0:
         rot.append((image).astype('float32'))
 
-#print rot.shape
-
-
-
-#buff=buffer_to_jet(,1,max_entry=100000, pix=25)
-    #rot.append(buff)
-
-
 
 buffdtype = [('images', 'float32')]
-
-
-
 df= np.array(rot,dtype=buffdtype)
-print df.shape
+
 #plot_mean_jet(df).savefig('test_rotate_avg.pdf')
-
-
-np.save('rotate_Wprime',df)
+np.save('rotate_QCD',df)
